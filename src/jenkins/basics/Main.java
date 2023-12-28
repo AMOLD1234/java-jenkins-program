215ff05d0af057242ef308b2530d7082365c15cb
Amol Dhole <amoldhole2572@gmail.com>
2023-12-29 01:38:40 +0530

    added Main.java class

---
+++ src/jenkins/basics/Main.java
@@ -0,0 +1,10 @@
+package jenkins.basics;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("This is java program...");
+		System.out.println("Output from java program.");
+	}
+
+}
